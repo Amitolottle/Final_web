3a46f5fd925d548666becbf783d15833ae588509
Psicodead <peperamon1993@hotmail.com>
2014-10-12 15:52:42 -0500

    img y css
    archivo base para css y algunas imagenes exportadas

---
+++ www/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Log In</title>
+	<link rel="stylesheet" href="css/styles.css"/>
+	<meta name= "viewport" content="width=device-width">
+	</head>
+	<body>
+		 <form action="includes/login.php" method="POST">
+			<label>Id:</label> <input type="text" name="Id" placeholder="ID"><br>
+			<!--Boton para iniciar sesion-->
+			<input type="submit" value="Log in">
+		</form>  
+	</body
+></html>
