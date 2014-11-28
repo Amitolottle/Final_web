f31a871182d4752468d611c93bfdfaad91009ee8
sebastianvcruz <sebastianvcruz@gmail.com>
2014-11-27 23:21:06 -0500

    Filtros2
    solo funciona historias activas

--- www/js/main.js
+++ www/js/main.js
@@ -73,7 +73,7 @@ $(document).ready(function(){
 		}).done(function(){
 			console.log("Solicitud enviada al API");
 		}).success(function(result){
-				window.location.replace("index.php");
+			window.location.replace("index.php");
 		}).error(function(error){
 			console.log("Error: "+ error);
 			//
@@ -86,60 +86,128 @@ $(document).ready(function(){
 		var value = $(this).val();
 		//console.log(value);
 		switch(value){
-			case "nuevas historias":
- 				$("#todoContenido").empty();
-			break;
-			case "":
- 				//$("#todoContenido").empty();
-			break;
-			case "nuevas historias":
- 				//$("#todoContenido").empty();
-			break;
-		}
-		
+			case "Activas":
+			$("#todoContenido").empty();
 
-		
-	});
-
-	$( ".linkHist" ).click(function( event ) {
-
-		console.log("ID Historia: "+this.id);
-		var idHis=this.id;
-		event.preventDefault();
 			$.ajax({
-			type: "POST",
-			url: "includes/agregarPluma.php",
-			data: { idHistoria: idHis}
-		}).done(function(){
-			console.log("Solicitud enviada al API");
-		}).success(function(result){
-			console.log("Resultado: "+result);
-			location.reload(false);
-		}).error(function(error){
-			console.log("Error: "+ error);
-			//
-		})
-	});
+				type: "GET",
+				url: "includes/filtrarActivo.php",
+				data: {}
+			})
+			.done(function(){
+				console.log("Solicitud enviada al API");
+			})
+			.success(function(result){
+				resultado = JSON.parse(result);
+				html = "";
+				if(!resultado.error){
+					for(i=0;i<resultado.historia.length;i++){ 
+						html += "<article class='row'>";
+						html += "<div class='cupos col-xs-4'>";
+						html +="<div class='frente info col-xs-11 "+resultado.historia[i].categoria+"'>";
+						html +="<h5>"+resultado.historia[i].cupos+" cupos disponibles</h5>";
+						html +="</div>";
+						html +="<div class='colaborar col-xs-12 "+resultado.historia[i].categoria+"'>";
+						html +="<h5 class='frente col-xs-8'>Colaborar</h5>";
+						html +="<a href='includes/validarContribucion.php?idHistoria="+resultado.historia[i].idHistoria+"'><figure class='col-xs-2'><img src='img/btn_mas.png' alt=''></figure></a>";
+						html +="<div class='col-xs-2'></div>";
+						html +="</div>";
+						html +="</div>";
+						html +="<div class='col-xs-4'></div>";
+						html +="<div class='img_perfil col-xs-5'>";
+						html +="<figure class='"+resultado.historia[i].categoria+"'><img src='"+resultado.historia[i].imagen+"' alt=''></figure>";
+						html +="</div>";
+						html +="<div class='frente plumas col-xs-4 "+resultado.historia[i].categoria+"'>";
+						html += "<a id='"+resultado.historia[i].idHistoria+"' class='linkHist' href='#'><figure class='col-xs-2'><img src='img/pluma_icon.png' alt=''></figure>"+resultado.historia[i].plumas+" plumas</a>";
+						html +="</div>";
+						html +="<div class='info_historia col-xs-12 "+resultado.historia[i].categoria+"'>";
+						html +="<div class='frente info col-xs-5'>";
+						html +="<div id='estado_historia' class='col-xs-2'></div>";
+						html +="<h5>Historia en curso</h5>";
+						html +="</div>";
+						html +="<div class='col-xs-2'></div>";
+						html +="<div class='frente info col-xs-5'>";
+						html +="<h5>tiempo restante: "+resultado.historia[i].tiempo+"</h5>";
+						html +="</div>";
+						html +="<div class='cont_info col-xs-12'>";
+						html +="<h1>"+resultado.historia[i].titulo+", por: "+resultado.historia[i].creador+"</h1>";
 
-	$( "#dejarComent" ).submit(function( event ) {
-		console.log("FUNCAComent");
-		var idHis=$("#idHistoriaActual").val();
-		var cont=$("#contenidoCom").val();
-		event.preventDefault();
-		$.ajax({
-			type: "POST",
-			url: "includes/agregarComentario.php",
-			data: { idHistoriaActual: idHis, contenidoCom: cont }
-		}).done(function(){
-			console.log("Solicitud enviada al API");
-		}).success(function(result){
-			console.log("Resultado: "+result);
-			location.reload(false);
-		}).error(function(error){
-			console.log("Error: "+ error);
+						html +="<h3>"+resultado.historia[i].creador+":</h3>";
+						html +="<p>"+resultado.historia[i].contenido+"</p>";
+
+						html +="<div class='col-xs-10'></div>";
+						html +="<a class='col-xs-2' href='visualizacion.php?idHistoria="+resultado.historia[i].idHistoria+"'>Ver más</a>";
+						html +="</div>";
+						html +="<div class='col-xs-12 clasificacion'>";
+						html +="<div class='col-xs-2'></div>";
+						html +="<div class='col-xs-4 tipo_historia'>";
+						html +="<figure class='col-xs-4'><img src='img/cuento_icon.png' alt=''></figure>";
+						html +="<h5 class='col-xs-8'>"+resultado.historia[i].tipo+"</h5> ";
+						html +="</div>";
+						html +="<div class='col-xs-4 genero_historia'>";
+						html +=" <figure class='col-xs-4'><img src='img/comedia_icon.png' alt=''></figure>";
+						html +="<h5 class='col-xs-8'>"+resultado.historia[i].categoria+"</h5> ";
+						html +="</div>";
+						html +="<div class='col-xs-2'></div>";
+						html +="</div>";
+						html +="</div>";
+						html +="</article>";
+                 			// html += "<a id='"+resultado.historia[i].idHistoria+"' class='linkHist' href='#'><figure class='col-xs-2'><img src='img/pluma_icon.png' alt=''></figure>".$filasPlumas." plumas</a>";
+                 			$("#todoContenido").html(html);
+                 		}
+                 	}
+                 	console.log(resultado);
+                 })
+.error(function(error){
+	console.log("Error: "+ error);
+})
+break;
+
+}
+
+
+
+});
+
+$( ".linkHist" ).click(function( event ) {
+
+	console.log("ID Historia: "+this.id);
+	var idHis=this.id;
+	event.preventDefault();
+	$.ajax({
+		type: "POST",
+		url: "includes/agregarPluma.php",
+		data: { idHistoria: idHis}
+	}).done(function(){
+		console.log("Solicitud enviada al API");
+	}).success(function(result){
+		console.log("Resultado: "+result);
+		location.reload(false);
+	}).error(function(error){
+		console.log("Error: "+ error);
 			//
 		})
-	});
-	
+});
+
+$( "#dejarComent" ).submit(function( event ) {
+	console.log("FUNCAComent");
+	var idHis=$("#idHistoriaActual").val();
+	var cont=$("#contenidoCom").val();
+	event.preventDefault();
+	$.ajax({
+		type: "POST",
+		url: "includes/agregarComentario.php",
+		data: { idHistoriaActual: idHis, contenidoCom: cont }
+	}).done(function(){
+		console.log("Solicitud enviada al API");
+	}).success(function(result){
+		console.log("Resultado: "+result);
+		location.reload(false);
+	}).error(function(error){
+		console.log("Error: "+ error);
+			//
+		})
+});
+
 });
 
